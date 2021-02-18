f0338b28f7fbaab4a1806887ca97b4a923bb263b
gopikrishhnachokkalamani <gopikrishhna@gmail.com>
2021-02-17 22:35:51 -0500

    intial

--- src/com/main/java/singleton/SingletonMain.java
+++ src/com/main/java/singleton/SingletonMain.java
@@ -2,6 +2,9 @@ package com.main.java.singleton;
 
 public class SingletonMain {
 
+  //Singleton pattern restricts the instantiation of a class and ensures that only
+  // one instance of the class exists in the java virtual machine
+  //Singleton pattern is used for logging, drivers objects, caching and thread pool.
   public static void main(String[] args) {
     //Singleton - 1
     EagerStudent student = EagerStudent.getInstance();
