cc6cb2698c78813339b9aa917b05b62c38603ddb
gopikrishhnachokkalamani <gopikrishhna@gmail.com>
2021-03-28 21:34:29 -0400

    intial

--- src/com/main/java/concurrency/executors/CompletableFutureMain.java
+++ src/com/main/java/concurrency/executors/CompletableFutureMain.java
@@ -9,7 +9,7 @@ public class CompletableFutureMain {
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-    //executed aysnchronsly, if no executor is specififed, then it will uses the common fork join pool implementation
+    //executed aysnchrounsly, if no executor is specified, then it will uses the common fork join pool implementation
     //which will use a daemon thread to run the runnable
     CompletableFuture<Void> runAsync = CompletableFuture.runAsync(() -> {Thread.currentThread().isDaemon(); try {
       Thread.sleep(3000);
